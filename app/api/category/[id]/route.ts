a5e8e895a5d5c94837ac0661e4f78f1fa15c340c
fikriii-pixel <qinshihuang266@gmail.com>
2025-08-03 09:28:27 +0700

    p

--- app/api/category/[id]/route.ts
+++ app/api/category/[id]/route.ts
@@ -1,13 +1,19 @@
 import prisma from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest, { params }: { params: { id: string } }) {
+export async function POST(
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
+  const { params } = context;
   const formData = await req.formData();
   const method = formData.get("_method");
 
   if (method === "PUT") {
     const name = formData.get("name") as string;
-    if (!name) return NextResponse.json({ message: "Nama tidak boleh kosong" }, { status: 400 });
+    if (!name) {
+      return NextResponse.json({ message: "Nama tidak boleh kosong" }, { status: 400 });
+    }
 
     await prisma.category.update({
       where: { id: params.id },
